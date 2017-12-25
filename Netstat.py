98550f7e057aa92fd15379568840132dc759126a
SmokeDog88 <34819287+SmokeDog88@users.noreply.github.com>
2017-12-24 17:38:08 -0800

    Check for open and listening connections

---
+++ Netstat.py
@@ -0,0 +1,43 @@
+# Written by Miguel Bigueur
+# Netstat
+# Security Scripting w/Python
+# Dec 24, 2017
+
+
+
+import socket
+from socket import AF_INET, SOCK_STREAM, SOCK_DGRAM
+import psutil
+
+AD = "-"
+AF_INET6 = getattr(socket, 'AF_INET6', object())
+proto_map = {
+    (AF_INET, SOCK_STREAM): 'tcp',
+    (AF_INET6, SOCK_STREAM): 'tcp6',
+    (AF_INET, SOCK_DGRAM): 'udp',
+    (AF_INET6, SOCK_DGRAM): 'udp6',
+}
+
+def main():
+    templ = "%-5s %-30s %-30s %-13s %-6s %s"
+    print(templ % (
+        "Proto", "Local address", "Remote address", "Status", "PID",
+        "Program name"))
+    proc_names = {}
+    for p in psutil.process_iter(attrs=['pid', 'name']):
+        proc_names[p.info['pid']] = p.info['name']
+    for c in psutil.net_connections(kind='inet'):
+        laddr = "%s:%s" % (c.laddr)
+        raddr = ""
+        if c.raddr:
+            raddr = "%s:%s" % (c.raddr)
+        print(templ % (
+            proto_map[(c.family, c.type)],
+            laddr,
+            raddr or AD,
+            c.status,
+            c.pid or AD,
+            proc_names.get(c.pid, '?')[:15],
+        ))
+if __name__ == '__main__':
+main()
