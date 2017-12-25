34577ff0290bdbf00953726ce4f5e780751aa177
SmokeDog88 <34819287+SmokeDog88@users.noreply.github.com>
2017-12-24 17:16:13 -0800

    Add files via upload

---
+++ Week5 Assgmt_Part1.py
@@ -0,0 +1,23 @@
+# OPSC 540 Security Scripting w/Python
+# Miguel Bigueur
+# Week5 Assignment Part 1
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# first: acquire full html content from web page
+html = urlopen("https://www.champlain.edu/current-students")
+
+# then read and parse html code and save into a variable
+bsObj = BeautifulSoup(html.read(), "html.parser")
+
+# use tag and id to isolate specific targeted data
+link = bsObj.findAll("div", {"id":"audience-nav"})
+
+'''iterate through lines containing li looking for a tags. 
+iterate once more through output for a tags and print out line
+with a tags, this also creates newline due to the looping iteration'''
+for li in link:
+    output = li.findAll("a")
+    for a in output:
+        print(a)
