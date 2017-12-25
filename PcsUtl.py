1676eb62368318fe03b7ab2605c22758fcdbdaee
SmokeDog88 <34819287+SmokeDog88@users.noreply.github.com>
2017-12-24 17:36:43 -0800

    Process Utility

---
+++ PcsUtl.py
@@ -0,0 +1,39 @@
+# Written by Miguel Bigueur
+# Process Utility
+# Security Scripting w/Python
+# Dec 24, 2017
+
+
+import psutil
+def bytes2human(n):
+    # http://code.activestate.com/recipes/578019
+    # >>> bytes2human(10000)
+    # '9.8K'
+    # >>> bytes2human(100001221)
+    # '95.4M'
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 1 << (i + 1) * 10
+    for s in reversed(symbols):
+        if n >= prefix[s]:
+            value = float(n) / prefix[s]
+            return '%.1f%s' % (value, s)
+    return "%sB" % n
+
+def pprint_ntuple(nt):
+    for name in nt._fields:
+        value = getattr(nt, name)
+        if name != 'percent':
+            value = bytes2human(value)
+        print('%-10s : %7s' % (name.capitalize(), value))
+
+def main():
+    print('MEMORY\n------')
+    pprint_ntuple(psutil.virtual_memory())
+    print('\nSWAP\n----')
+    pprint_ntuple(psutil.swap_memory())
+
+
+if __name__ == '__main__':
+main()
